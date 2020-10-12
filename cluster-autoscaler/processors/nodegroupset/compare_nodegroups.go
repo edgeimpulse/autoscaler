9d17ff5d3afc3f721e4f83b0c4520906c15e5916
Mathijs Baaijens <mathijs@mathijsbaaijens.nl>
2020-10-12 23:11:16 +0200

    Lets ignore labels with our own magic 'edgeimpulse.io' prefix.

--- cluster-autoscaler/processors/nodegroupset/compare_nodegroups.go
+++ cluster-autoscaler/processors/nodegroupset/compare_nodegroups.go
@@ -18,6 +18,7 @@ package nodegroupset
 
 import (
 	"math"
+	"strings"
 
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -77,6 +78,9 @@ func compareLabels(nodes []*schedulerframework.NodeInfo, ignoredLabels map[strin
 	for _, node := range nodes {
 		for label, value := range node.Node().ObjectMeta.Labels {
 			ignore, _ := ignoredLabels[label]
+			if strings.HasPrefix(label, "edgeimpulse.io") {
+				ignore = true
+			}
 			if !ignore {
 				labels[label] = append(labels[label], value)
 			}
